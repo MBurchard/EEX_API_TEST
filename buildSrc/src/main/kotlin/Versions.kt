b356257a964f1142d015f093033184e3ae445d75
Martin Burchard <dev@burchard-it.de>
2020-11-20 12:09:00 +0100

    Kotlin version update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val APACHE_CSV = "1.8"
     const val BEN_MANES_VERSIONS = "0.36.0"
-    const val KOTLIN = "1.4.10"
+    const val KOTLIN = "1.4.20"
     const val KOTLIN_LOGGING = "2.0.3"
     const val SPRING_BOOT = "2.4.0"
     const val SPRING_DEPENDENCY_MANAGEMENT = "1.0.10.RELEASE"
